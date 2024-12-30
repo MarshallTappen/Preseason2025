3451384fdd69a244ca33ac8b19634a02851e0c7b
Marshall Tappen <mtappen@gmail.com>
2024-12-29 17:26:43 -0800

    Added a basic, two-option SendableChooser for Autos

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,9 +14,14 @@ import frc.robot.subsystems.vision.TargetDetectorInterface;
 
 import java.io.File;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -42,6 +47,7 @@ public class RobotContainer {
   public final SwerveTeleopCommand swerveTeleopCommand = new SwerveTeleopCommand(swerveSubsystem, driverController);
   // Replace with CommandPS4Controller or CommandJoystick if needed
 
+  private final SendableChooser<Command> autoChooser;
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
@@ -55,6 +61,11 @@ public class RobotContainer {
 
     // Configure the trigger bindings
     configureBindings();
+
+    // Need to initialize this here after vision is configured.
+    // Need to clean up initialization flow to make it more clear
+    autoChooser = buildAutoChooser();
+
   }
 
   /**
@@ -85,9 +96,20 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    // return Autos.moveToPointAuto(swerveSubsystem);
 
-    return Autos.driveToNote(swerveSubsystem, noteDetector);
+    // return Autos.driveToNote(swerveSubsystem, noteDetector);
+    return autoChooser.getSelected();
+
   }
+
+  private SendableChooser<Command> buildAutoChooser() {
+    SendableChooser<Command> chooser = new SendableChooser<Command>();
+
+    chooser.setDefaultOption("None", Commands.none());
+    chooser.addOption("DriveToNote", Autos.driveToNote(swerveSubsystem, noteDetector));
+    SmartDashboard.putData("Auto Chooser", chooser);
+
+    return chooser;
+  }
+
 }
